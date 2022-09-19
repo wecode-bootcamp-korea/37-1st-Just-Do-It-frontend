5171a84f282b778b413751ff3d001a4fd7d5bacd
1211lys <hpppower111@gmail.com>
2022-09-19 17:12:36 +0900

    ADD : 초기세팅 2차수정

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 import Main from './pages/Main/Main';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
