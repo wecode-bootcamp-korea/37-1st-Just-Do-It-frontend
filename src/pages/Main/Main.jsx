ccd2e9d7563bbea5c588c2c8231cc76a35f79f80
1211lys <hpppower111@gmail.com>
2022-09-21 21:38:09 +0900

    REF : 상수데이터 수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.scss';
 
 function Main() {
+  const [moveIndex, setMoveIndex] = useState(0);
+
+  const leftMove = () => {
+    if (moveIndex === 0) {
+      return;
+    }
+    setMoveIndex(prev => prev + 10);
+  };
+  const rightMove = () => {
+    setMoveIndex(prev => prev - 10);
+  };
   return (
     <div>
       <div className="mainFirstImgWrapper">
-        <h1 className="mainFirstImgTitle">Trending Now</h1>
+        <h1 className="mainFirstImgTitle categoryText">Trending Now</h1>
         <div className="mainFristImgContainer">
           <div className="mainFirstImgBox" />
           <div className="mainSecondImgBox">
@@ -13,10 +24,28 @@ function Main() {
             <div className="mainThirdImg" />
           </div>
         </div>
-        <h1 className="mainSecondImgTitle">Trending Now</h1>
+        <h1 className="mainSecondImgTitle categoryText">Trending Now</h1>
         <div className="mainSecondImgContainer">
           <div className="mainFirstImgBox" />
           <div className="mainSecondImgBox" />
+        </div>
+        <h1 className="mainThirdImgTitle categoryText">Trending Now</h1>
+        <button onClick={leftMove}>left</button>
+        <button onClick={rightMove}>right</button>
+        <div
+          className="thirdTitle"
+          style={{ transform: `translateX(${moveIndex}%)` }}
+        >
+          {MAIN_FOOTER_IMG_DATA.map((e, idx) => {
+            return (
+              <div className="thirdImgBox" key={idx}>
+                <img className="thirdImg" src={e.img} alt="#" />
+                <h1 className="categoryText">{e.name}</h1>
+                <p className="defaultText">{e.type}</p>
+                <p className="defaultText">{e.price}원</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
@@ -24,3 +53,60 @@ function Main() {
 }
 
 export default Main;
+
+const MAIN_FOOTER_IMG_DATA = [
+  {
+    name: '고양이1',
+    img: 'https://www.shutterstock.com/ko/blog/wp-content/uploads/sites/17/2021/04/dog-portrait-4.jpeg?w=750',
+    type: '남성 신발',
+    price: '139,000',
+  },
+  {
+    name: '고양이2',
+    img: 'https://www.shutterstock.com/ko/blog/wp-content/uploads/sites/17/2021/04/dog-portrait-4.jpeg?w=750',
+    type: '남성 신발',
+    price: '139,000',
+  },
+  {
+    name: '고양이3',
+    img: 'https://www.shutterstock.com/ko/blog/wp-content/uploads/sites/17/2021/04/dog-portrait-4.jpeg?w=750',
+    type: '남성 신발',
+    price: '139,000',
+  },
+  {
+    name: '고양이4',
+    img: 'https://www.shutterstock.com/ko/blog/wp-content/uploads/sites/17/2021/04/dog-portrait-4.jpeg?w=750',
+    type: '남성 신발',
+    price: '139,000',
+  },
+  {
+    name: '고양이5',
+    img: 'https://www.shutterstock.com/ko/blog/wp-content/uploads/sites/17/2021/04/dog-portrait-4.jpeg?w=750',
+    type: '남성 신발',
+    price: '139,000',
+  },
+  {
+    name: '고양이',
+    img: 'https://www.shutterstock.com/ko/blog/wp-content/uploads/sites/17/2021/04/dog-portrait-4.jpeg?w=750',
+    type: '남성 신발',
+    price: '139,000',
+  },
+  {
+    name: '고양이',
+    img: 'https://www.shutterstock.com/ko/blog/wp-content/uploads/sites/17/2021/04/dog-portrait-4.jpeg?w=750',
+    type: '남성 신발',
+    price: '139,000',
+  },
+  {
+    name: '고양이',
+    img: 'https://www.shutterstock.com/ko/blog/wp-content/uploads/sites/17/2021/04/dog-portrait-4.jpeg?w=750',
+    type: '남성 신발',
+    price: '139,000',
+  },
+  {
+    name: '고양이',
+    img: 'https://www.shutterstock.com/ko/blog/wp-content/uploads/sites/17/2021/04/dog-portrait-4.jpeg?w=750',
+    type: '남성 신발',
+    price: '139,000',
+  },
+];
