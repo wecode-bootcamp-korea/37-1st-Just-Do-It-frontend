0f99b4c1a97fadc8dd21c0c4dfbc0c01590512da
1211lys <hpppower111@gmail.com>
2022-09-21 17:31:22 +0900

    REF : 상수데이터, 클래스네임 수정

--- src/components/ViewItem/ViewItem.jsx
+++ src/components/ViewItem/ViewItem.jsx
@@ -4,111 +4,211 @@ import './ViewItem.scss';
 
 function ViewItem() {
   return (
-    <div className="ViewItemWrapper">
+    <div className="viewitem">
       <div>
-        <Link className="blockTitle" to="/item-list">
-          New & Featured
-        </Link>
-        <Link className="block" to="/item-list">
-          신상품 전체보기
-        </Link>
-        <Link className="block" to="/item-list">
-          SNKRS
-        </Link>
-        <Link className="block" to="/item-list">
-          나이키 덩크
-        </Link>
-        <Link className="block" to="/item-list">
-          ACG
-        </Link>
+        {NEW_FEATURED_DATA.map((e, idx) => {
+          return (
+            <Link key={idx} className="defaultText" to="/item-list">
+              {e.name}
+            </Link>
+          );
+        })}
       </div>
       <div>
-        <Link className="blockTitle" to="/item-list">
-          New & Featured
-        </Link>
-        <Link className="block" to="/item-list">
-          신상품 전체보기
-        </Link>
-        <Link className="block" to="/item-list">
-          SNKRS
-        </Link>
-        <Link className="block" to="/item-list">
-          나이키 덩크
-        </Link>
-        <Link className="block" to="/item-list">
-          ACG
-        </Link>
+        {SHOES_DATA.map((e, idx) => {
+          return (
+            <Link key={idx} className="defaultText" to="/item-list">
+              {e.name}
+            </Link>
+          );
+        })}
+      </div>
+      <div className="clothesBox">
+        {CLOTHES_DATA.map((e, idx) => {
+          return (
+            <Link key={idx} className="defaultText" to="/item-list">
+              {e.name}
+            </Link>
+          );
+        })}
       </div>
       <div>
-        <Link className="blockTitle" to="/item-list">
-          New For Men
-        </Link>
-        <Link className="block" to="/item-list">
-          신상품 전체보기
-        </Link>
-        <Link className="block" to="/item-list">
-          SNKRS
-        </Link>
-        <Link className="block" to="/item-list">
-          나이키 덩크
-        </Link>
-        <Link className="block" to="/item-list">
-          ACG
-        </Link>
+        {SPORTS_DATA.map((e, idx) => {
+          return (
+            <Link key={idx} className="defaultText" to="/item-list">
+              {e.name}
+            </Link>
+          );
+        })}
       </div>
       <div>
-        <Link className="blockTitle" to="/item-list">
-          New For Women
-        </Link>
-        <Link className="block" to="/item-list">
-          신상품 전체보기
-        </Link>
-        <Link className="block" to="/item-list">
-          SNKRS
-        </Link>
-        <Link className="block" to="/item-list">
-          나이키 덩크
-        </Link>
-        <Link className="block" to="/item-list">
-          ACG
-        </Link>
-      </div>
-      <div>
-        <Link className="blockTitle" to="/item-list">
-          New For Kids
-        </Link>
-        <Link className="block" to="/item-list">
-          신상품 전체보기
-        </Link>
-        <Link className="block" to="/item-list">
-          SNKRS
-        </Link>
-        <Link className="block" to="/item-list">
-          나이키 덩크
-        </Link>
-        <Link className="block" to="/item-list">
-          ACG
-        </Link>
-      </div>
-      <div>
-        <Link className="blockTitle" to="/item-list">
-          Sale
-        </Link>
-        <Link className="block" to="/item-list">
-          신상품 전체보기
-        </Link>
-        <Link className="block" to="/item-list">
-          SNKRS
-        </Link>
-        <Link className="block" to="/item-list">
-          나이키 덩크
-        </Link>
-        <Link className="block" to="/item-list">
-          ACG
-        </Link>
+        {BRAND_DATA.map((e, idx) => {
+          return (
+            <Link key={idx} className="defaultText" to="/item-list">
+              {e.name}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
 }
 
 export default ViewItem;
+const NEW_FEATURED_DATA = [
+  {
+    name: 'New & Featured',
+  },
+  {
+    name: '신상품',
+  },
+  {
+    name: 'SNKRS',
+  },
+  {
+    name: 'MUST HAVES',
+  },
+  {
+    name: 'MEMBER SHOP',
+  },
+  {
+    name: '젠더리스 컬렉션',
+  },
+  {
+    name: '애슬릿 리뷰',
+  },
+  {
+    name: '가을을 위한 스타일링 추천',
+  },
+  {
+    name: '지속 가능 컬렉션',
+  },
+  {
+    name: '가을 컬렉션',
+  },
+  {
+    name: '품절임박',
+  },
+];
+const SHOES_DATA = [
+  {
+    name: '신발 전체',
+  },
+  {
+    name: '라이프스타일',
+  },
+  {
+    name: '러닝',
+  },
+  {
+    name: '워킹',
+  },
+  {
+    name: '트레이닝 & 짐',
+  },
+  {
+    name: '농구',
+  },
+  {
+    name: '조던',
+  },
+  {
+    name: '축구',
+  },
+  {
+    name: '스케이트보딩',
+  },
+  {
+    name: '골프',
+  },
+  {
+    name: '테니스',
+  },
+  {
+    name: '샌들 & 슬리퍼',
+  },
+];
+
+const CLOTHES_DATA = [
+  {
+    name: '의류 전체',
+  },
+  {
+    name: '후드 & 크루',
+  },
+  {
+    name: '재킷 & 베스트',
+  },
+  {
+    name: '팬츠 & 타이츠',
+  },
+  {
+    name: '탑 & 티셔츠',
+  },
+  {
+    name: '숏 팬츠',
+  },
+  {
+    name: '나이키 프로',
+  },
+  {
+    name: '양말',
+  },
+  {
+    name: '셋업',
+  },
+  {
+    name: '용품',
+  },
+  {
+    name: '모자 & 헤드밴드',
+  },
+  {
+    name: '가방',
+  },
+  {
+    name: '애플워치',
+  },
+];
+
+const SPORTS_DATA = [
+  {
+    name: '러닝',
+  },
+  {
+    name: '트레이닝 & 짐',
+  },
+  {
+    name: '농구',
+  },
+  {
+    name: '축구',
+  },
+  {
+    name: '스케이트보딩',
+  },
+  {
+    name: '골프',
+  },
+  {
+    name: '테니스',
+  },
+];
+
+const BRAND_DATA = [
+  {
+    name: 'Nike Sportswear',
+  },
+  {
+    name: 'NikeLab',
+  },
+  {
+    name: 'Jordan',
+  },
+  {
+    name: 'NAB',
+  },
+  {
+    name: 'ACG',
+  },
+];
