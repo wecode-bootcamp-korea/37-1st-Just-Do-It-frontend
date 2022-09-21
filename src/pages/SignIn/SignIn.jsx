c6a62952a2b7ee5b19a8ba889e1d71a5650ef92f
1211lys <hpppower111@gmail.com>
2022-09-21 10:33:04 +0900

    ADD : Signin Layout

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import './SignIn.scss';
 
 function SignIN() {
-  return <h1>SignIN</h1>;
+  return (
+    <div className="SignInWrapper">
+      <h1 className="SignInTitle">나이키 멤버 가입</h1>
+      <p className="SignInInfo">
+        멤버가 되어 나이키가 제공하는
+        <br />
+        최고의 제품과 혜택을 만나보세요.
+      </p>
+      <form>
+        <input className="SignInInput" type="text" placeholder="ID" />
+        <input className="SignInInput" type="text" placeholder="P/W" />
+        <input
+          className="SignInInput"
+          type="text"
+          placeholder="P/W 다시 입력"
+        />
+        <input className="SignInInput" type="text" placeholder="주소" />
+        <input className="SignInInput" type="text" placeholder="이름" />
+        <button className="SignInButtonMan">남성</button>
+        <button className="SignInButtonWoman">여성</button>
+        <input className="SignInInput" type="text" placeholder="생년월일" />
+        <button className="SignInButtonSignIn">
+          회원가입하기 (만 14세 이상)
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SignIN;
