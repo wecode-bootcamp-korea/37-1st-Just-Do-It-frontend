133425735a7b092a8e6cbfe357dd443767a826b3
1211lys <hpppower111@gmail.com>
2022-09-21 10:20:26 +0900

    ADD : Main Layout

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import './Main.scss';
 
 function Main() {
-  return <h1>Main</h1>;
+  return (
+    <div>
+      <div className="mainFirstImgWrapper">
+        <h1 className="mainFirstImgTitle">Trending Now</h1>
+        <div className="mainFristImgContainer">
+          <div className="mainFirstImgBox" />
+          <div className="mainSecondImgBox">
+            <div className="mainSecondImg" />
+            <div className="mainThirdImg" />
+          </div>
+        </div>
+        <h1 className="mainSecondImgTitle">Trending Now</h1>
+        <div className="mainSecondImgContainer">
+          <div className="mainFirstImgBox" />
+          <div className="mainSecondImgBox" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
