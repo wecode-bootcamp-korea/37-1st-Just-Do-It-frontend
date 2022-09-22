fbfa1f5c119e6c516630e183804e07ca40ca018b
1211lys <hpppower111@gmail.com>
2022-09-22 09:18:40 +0900

    ???

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import Main from './pages/Main/Main';
+import ItemList from './pages/ItemsList/ItemsList';
+import SignIn from './pages/SignIn/SignIn';
+import Login from './components/Login/Login';
+import SearchModal from './components/SearchModal/SearchModal';
+import ViewItem from './components/ViewItem/ViewItem';
 
 function Router() {
   return (
@@ -10,6 +15,11 @@ function Router() {
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/item-list" element={<ItemList />} />
+        <Route path="/search-modal" element={<SearchModal />} />
+        <Route path="/view-item" element={<ViewItem />} />
       </Routes>
       <Footer />
     </BrowserRouter>
