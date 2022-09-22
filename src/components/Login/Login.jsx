10d6aefa950584e8f5badb6c2ab475157f363329
1211lys <hpppower111@gmail.com>
2022-09-23 08:38:59 +0900

    feat(Login): 로그인 API 함수 구현

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -31,7 +31,7 @@ function Login() {
         }
         throw new Error('에러 발생!');
       })
-      // .catch((error) => console.log(error))
+      .catch(error => console.log(error))
       .then(data => {
         localStorage.setItem('TOKEN', data.accessToken);
 
@@ -39,57 +39,54 @@ function Login() {
       });
   };
   return (
-    <div className="loginWrapper">
-      <div className="formContainer">
-        <form action="">
-          <button className="loginModalDelete">
-            <i className="fa-solid fa-x" />
+    <div className="login">
+      <form action="">
+        <button className="loginModalDelete">
+          <i className="fa-solid fa-x" />
+        </button>
+        <p className="loginIcon">나이키</p>
+        <h1 className="loginTitle">나이키 로그인</h1>
+        <input
+          className="loginId block"
+          name="username"
+          type="text"
+          placeholder="아이디"
+          onChange={handleChangeInput}
+          value={inputValue.username}
+        />
+        <input
+          className="loginPw block"
+          name="password"
+          type="password"
+          placeholder="비밀번호"
+          onChange={handleChangeInput}
+          value={inputValue.password}
+        />
+        <div className="loginHelp">
+          <button className="block">
+            <input type="checkbox" className="checkBox" />
+            로그인 유지하기
           </button>
-          <p className="loginIcon">나이키</p>
-          <h1 className="loginTitle">나이키 로그인</h1>
-          <input
-            className="loginId block"
-            name="username"
-            type="text"
-            placeholder="아이디"
-            onChange={handleChangeInput}
-            value={inputValue.username}
-          />
-          <input
-            className="loginPw block"
-            name="password"
-            type="password"
-            placeholder="비밀번호"
-            onChange={handleChangeInput}
-            value={inputValue.password}
-          />
-          <button className="loginButton block" onClick={handleLogin}>
-            로그인
-          </button>
-          <div className="loginHelp">
-            <button className="block">
-              <input type="checkbox" className="checkBox" />
-              로그인 유지하기
-            </button>
-            <button className="block">아이디/비밀번호 찾기</button>
-          </div>
-
-          <button className="loginKakao block">
-            <i className="fa-solid fa-message" />
-            카카오계정 로그인
-          </button>
-          <button className="loginFacebook block">
-            <i className="fa-brands fa-facebook-f" />
-            페이스북으로 로그인
-          </button>
-          <p className="signInCheck">
-            회원이 아니신가요?
-            <Link to="/sign-in" className="signIn">
-              회원가입
-            </Link>
-          </p>
-        </form>
-      </div>
+          <button className="block">아이디/비밀번호 찾기</button>
+        </div>
+        <button className="loginButton block" onClick={handleLogin}>
+          로그인
+        </button>
+        <button className="loginKakao block">
+          <i className="fa-solid fa-message" />
+          카카오계정 로그인
+        </button>
+        <button className="loginFacebook block">
+          <i className="fa-brands fa-facebook-f" />
+          페이스북으로 로그인
+        </button>
+        <p className="signInCheck">
+          회원이 아니신가요?
+          <Link to="/sign-in" className="signIn">
+            회원가입
+          </Link>
+        </p>
+      </form>
     </div>
   );
 }
