909ba4831b3235fb6f834a0cd12032194268c0de
1211lys <hpppower111@gmail.com>
2022-09-23 17:34:54 +0900

    wip

--- src/Router.js
+++ src/Router.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import ViewItem from './components/ViewItem/ViewItem';
-import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
-import Main from './pages/Main/Main';
+// import Nav from './components/Nav/Nav';
+// import Footer from './components/Footer/Footer';
+// import Main from './pages/Main/Main';
 import ItemList from './pages/ItemsList/ItemsList';
-
+import SignIn from './pages/SignIn/SignIn';
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/view-item" element={<ViewItem />} />
-        <Route path="/" element={<Main />} />
+        <Route path="/signin" element={<SignIn />} />
+
+        {/* <Route path="/" element={<Main />} /> */}
         <Route path="/itemList" element={<ItemList />} />
       </Routes>
     </BrowserRouter>
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -5,12 +5,12 @@ import './SignIn.scss';
 function SignIn() {
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState({
-    username: '',
+    userName: '',
     password: '',
     passwordForCheck: '',
     address: '',
-    fullname: '',
-    phone_number: '',
+    fullName: '',
+    phoneNumber: '',
     birth: '',
     gender: '2',
   });
@@ -26,50 +26,74 @@ function SignIn() {
   const signInForm = document.getElementById('signInForm');
   const handleSignUp = e => {
     e.preventDefault();
-    const pwReg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+    const idReg = /^[a-zA-Z0-9_-]{6,99}$/;
+    const pwReg = /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,16})/;
     const birthReg =
       /^(19[0-9][0-9]|20\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
-    let mobileReg = /^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}/;
-    if (!(inputValue.username.length > 6)) return alert('ID가 짧음');
+    const mobileReg = /^[0-9]{2,3}[0-9]{3,4}[0-9]{4}/;
+    const today = new Date();
+    const todayTime = today.getTime();
+    const birthTime = new Date(inputValue.birth);
+    let ageReg = Math.floor(
+      (todayTime - birthTime) / (1000 * 60 * 60 * 24 * 365)
+    );
+    console.log('age', ageReg);
+
+    if (!idReg.test(inputValue.userName))
+      return alert('아이디 양식에 맞지 않음');
+    if (!(inputValue.userName.length > 6)) return alert('ID가 짧음');
     if (!pwReg.test(inputValue.password)) return alert('비밀번호가 잘못됨');
     if (!(inputValue.password === inputValue.passwordForCheck))
       return alert('비밀번호가 안같음');
-    if (!mobileReg.test(inputValue.phone_number)) return alert('폰번호 똑바로');
-    if (!birthReg.test(inputValue.birth)) return alert('15살이상만');
+    if (!mobileReg.test(inputValue.phoneNumber)) return alert('폰번호 똑바로');
+    if (!birthReg.test(inputValue.birth)) return alert('양식에 맞지 않아');
     if (inputValue.gender === '2') return alert('성별도 체크');
 
-    const formData = new FormData();
-    formData.append('username', inputValue.username);
-    formData.append('password', inputValue.password);
-    formData.append('passwordForCheck', inputValue.passwordForCheck);
-    formData.append('address', inputValue.address);
-    formData.append('fullname', inputValue.fullname);
-    formData.append('phone_number', inputValue.phone_number);
-    formData.append('birth', inputValue.birth);
-    formData.append('gender', Number(inputValue.gender));
+    // const formData = new FormData();
+    // formData.append('username', inputValue.userName);
+    // formData.append('password', inputValue.password);
+    // formData.append('passwordForCheck', inputValue.passwordForCheck);
+    // formData.append('address', inputValue.address);
+    // formData.append('fullname', inputValue.fullName);
+    // formData.append('phone_number', inputValue.phoneNumber);
+    // formData.append('birth', inputValue.birth);
+    // formData.append('gender', Number(inputValue.gender));
+
+    const submitData = {
+      userName: inputValue.userName,
+      password: inputValue.password,
+      address: inputValue.address,
+      fullName: inputValue.fullName,
+      phoneNumber: inputValue.phoneNumber,
+      birth: inputValue.birth,
+      gender: inputValue.gender,
+    };
+    console.log(submitData);
+
+    // const signInForm = document.getElementById('signInForm');
 
     // for (let key of formData.keys()) {
     //   console.log(key, ':', formData.get(key));
     // }
-    fetch('API', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      body: new FormData(signInForm),
-    })
-      .then(response => {
-        console.log('0', response);
+    // fetch('http://172.20.10.3:8000/users/signup', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json;charset=utf-8',
+    //   },
+    //   body: JSON.stringify(submitData),
+    // }).then(response => console.log(response));
+    // .then(response => {
+    //   console.log('0', response);
 
-        if (response.ok === true) {
-          console.log('1');
-          console.log('2', response.json());
+    //   if (response.ok === true) {
+    //     console.log('1');
+    //     console.log('2', response.json());
 
-          navigate('../Login/Login.jsx');
-        }
-        throw new Error('에러 발생!');
-      })
-      .catch(error => console.log('3', error));
+    //     navigate('../Login/Login.jsx');
+    //   }
+    //   throw new Error('에러 발생!');
+    // })
+    // .catch(error => console.log('3', error));
   };
 
   return (
@@ -83,7 +107,7 @@ function SignIn() {
       <form id="signInForm" onSubmit={handleSignUp}>
         <input
           className="SignInInput"
-          name="username"
+          name="userName"
           type="text"
           placeholder="사용하실 ID를 입력해주세요."
           onChange={handleChangeInput}
@@ -107,7 +131,7 @@ function SignIn() {
         />
         <input
           className="SignInInput"
-          name="fullname"
+          name="fullName"
           type="text"
           placeholder="이름을 입력해 주세요."
           onChange={handleChangeInput}
@@ -123,7 +147,7 @@ function SignIn() {
         />
         <input
           className="SignInInput"
-          name="phone_number"
+          name="phoneNumber"
           type="text"
           placeholder="휴대폰 번호 '-'표 없이 입력해 주세요."
           onChange={handleChangeInput}
