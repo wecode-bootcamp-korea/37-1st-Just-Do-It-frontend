e1b8b48e763b3a91fb36f6081f9441fc00ace9f5
1211lys <hpppower111@gmail.com>
2022-09-23 09:27:53 +0900

    refac(viewitem) : 스타일, 데이터 수정

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
-import Main from './pages/Main/Main';
 import ViewItem from './components/ViewItem/ViewItem';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path="/" element={<Main />} />
         <Route path="/view-item" element={<ViewItem />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
