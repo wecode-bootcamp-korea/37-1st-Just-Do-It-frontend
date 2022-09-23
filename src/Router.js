ffdec83dec1b5ccd0aebc8b504cefed8650fb008
1211lys <hpppower111@gmail.com>
2022-09-23 14:59:12 +0900

    잘못 수정

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import ViewItem from './components/ViewItem/ViewItem';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import Main from './pages/Main/Main';
 import ItemList from './pages/ItemsList/ItemsList';
-import SignIn from './pages/SignIn/SignIn';
-import Login from './components/Login/Login';
-import SearchModal from './components/SearchModal/SearchModal';
-import ViewItem from './components/ViewItem/ViewItem';
 
 function Router() {
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/item-list" element={<ItemList />} />
-        <Route path="/search-modal" element={<SearchModal />} />
         <Route path="/view-item" element={<ViewItem />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/itemList" element={<ItemList />} />
       </Routes>
       <Footer />
     </BrowserRouter>
