2d9cc716f74ad361ab414d92e24aa15b05455fcb
1211lys <hpppower111@gmail.com>
2022-09-25 20:46:10 +0900

    ADD : 메인 레이아웃

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,41 +1,86 @@
-import React from 'react';
-import { MAIN_FOOTER_IMG_DATA } from './MAIN_DATA';
+import React, { useEffect, useState } from 'react';
+// import { MAIN_FOOTER_IMG_DATA } from './MAIN_DATA';
 import './Main.scss';
 
 function Main() {
-  // useEffect(() => {
-  //   fetch('API').then(response => console.log(response));
-  // }, []);
+  const [itemList, setItemList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://172.20.10.12:8000/products/?offset=0&limit=19')
+      .then(response => response.json())
+      // .then(data => console.log(data))
+      .then(result => setItemList(result.list))
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
   return (
-    <div>
-      <div className="mainFirstImgWrapper">
-        <h1 className="mainFirstImgTitle categoryText">Trending Now</h1>
-        <div className="mainFristImgContainer">
-          <div className="mainFirstImgBox" />
-          <div className="mainSecondImgBox">
-            <div className="mainSecondImg" />
-            <div className="mainThirdImg" />
-          </div>
+    <div className="main">
+      <h1 className="videoImgTitle categoryText">
+        JUST-DO-IT 이승훈은 어떤사람일까 ?
+      </h1>
+      <div className="video">
+        <video muted autoPlay loop width="80%">
+          <source src="/videos/nikeRun33.mp4" type="video/mp4" alt="Nike Run" />
+        </video>
+      </div>
+      <div className="videoTextContainer">
+        <h1 className="infoHeader">이승훈의 달리기</h1>
+        <p className="videoInfo">
+          선선한 날씨를 위한 가을 컬렉션과 함께 데일리 룩을 완성해보세요.
+        </p>
+        <div className="videoButton">
+          <button className="buyButton btn">구매하기</button>
+          <button className="infoButton btn">자세히 보기</button>
         </div>
-        <h1 className="mainSecondImgTitle categoryText">Trending Now</h1>
-        <div className="mainSecondImgContainer">
-          <div className="mainFirstImgBox" />
-          <div className="mainSecondImgBox" />
+      </div>
+      <h1 className="firstImgTitle categoryText">이승훈의 신발</h1>
+      <div className="firstImgContainer">
+        <div className="firstImgBox" />
+        <div className="secondImgBox">
+          <div className="secondImg" />
+          <div className="mainThirdImg" />
         </div>
-        <h1 className="mainThirdImgTitle categoryText">Trending Now</h1>
-        <div className="thirdTitle">
-          {MAIN_FOOTER_IMG_DATA.map(e => {
+      </div>
+      <div className="videoTextContainer">
+        <h1 className="infoHeader">이승훈의 달리기</h1>
+        <p className="videoInfo">
+          선선한 날씨를 위한 가을 컬렉션과 함께 데일리 룩을 완성해보세요.
+        </p>
+        <div className="videoButton">
+          <button className="buyButton btn">구매하기</button>
+          <button className="infoButton btn">자세히 보기</button>
+        </div>
+      </div>
+      <h1 className="secondImgTitle categoryText">이승훈의 옷</h1>
+      <div className="secondImgContainer">
+        <div className="firstImgBox" />
+        <div className="secondImgBox" />
+      </div>
+      <div className="videoTextContainer">
+        <h1 className="infoHeader">이승훈의 달리기</h1>
+        <p className="videoInfo">
+          선선한 날씨를 위한 가을 컬렉션과 함께 데일리 룩을 완성해보세요.
+        </p>
+        <div className="videoButton">
+          <button className="buyButton btn">구매하기</button>
+          <button className="infoButton btn">자세히 보기</button>
+        </div>
+      </div>
+      <h1 className="thirdImgTitle categoryText">Featured Shoes</h1>
+      <div className="thirdTitle">
+        {itemList.map(
+          ({ id, thumbnail, productName, brandName, retailPrice }) => {
             return (
-              <div className="thirdImgBox" key={e.id}>
-                <img className="thirdImg" src={e.thumbnail} alt="#" />
-                <h1 className="categoryText">{e.productName}</h1>
-                <p className="defaultText">{e.brandName}</p>
-                <p className="defaultText mb40">{e.retailPrice}원</p>
+              <div className="thirdImgBox" key={id}>
+                <img className="thirdImg" src={thumbnail} alt="#" />
+                <h1 className="categoryText">{productName}</h1>
+                <p className="defaultText">{brandName}</p>
+                <p className="defaultText mb40">{retailPrice}원</p>
               </div>
             );
-          })}
-          <div className="scrollbar" />
-        </div>
+          }
+        )}
       </div>
     </div>
   );
