b93a8fc48332f27566b5ce60adbc160f06371748
1211lys <hpppower111@gmail.com>
2022-09-27 09:17:44 +0900

    refc 코드 수정

--- src/components/ViewItem/ViewItemModal.jsx
+++ src/components/ViewItem/ViewItemModal.jsx
@@ -1,62 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import {
   NEW_FEATURED_DATA,
   SHOES_DATA,
   CLOTHES_DATA,
   SPORTS_DATA,
   BRAND_DATA,
-} from './ViewItemData';
+} from './VIEW_ITEM_DATA';
 import './ViewItemModal.scss';
 
 function ViewItem({ closeTargetModal }) {
-  const handleMouseLeave = () => {
-    closeTargetModal('viewItem');
-  };
+  const handleMouseLeave = () => closeTargetModal('viewItem');
+
   return (
     <div className="viewItemWrapper" onMouseLeave={handleMouseLeave}>
       <div className="viewItem">
         <div>
-          {NEW_FEATURED_DATA.map((e, idx) => {
+          {NEW_FEATURED_DATA.map(({ featuredKey, name }) => {
             return (
-              <Link key={idx} className="defaultText" to="/item-list">
-                {e.name}
+              <Link key={featuredKey} className="defaultText" to="/item-list">
+                {name}
               </Link>
             );
           })}
         </div>
         <div>
-          {SHOES_DATA.map((e, idx) => {
+          {SHOES_DATA.map(({ shoesKey, name }) => {
             return (
-              <Link key={idx} className="defaultText" to="/item-list">
-                {e.name}
+              <Link key={shoesKey} className="defaultText" to="/item-list">
+                {name}
               </Link>
             );
           })}
         </div>
         <div className="clothesBox">
-          {CLOTHES_DATA.map((e, idx) => {
+          {CLOTHES_DATA.map(({ clothesKey, name }) => {
             return (
-              <Link key={idx} className="defaultText" to="/item-list">
-                {e.name}
+              <Link key={clothesKey} className="defaultText" to="/item-list">
+                {name}
               </Link>
             );
           })}
         </div>
         <div>
-          {SPORTS_DATA.map((e, idx) => {
+          {SPORTS_DATA.map(({ sportsKey, name }) => {
             return (
-              <Link key={idx} className="defaultText" to="/item-list">
-                {e.name}
+              <Link key={sportsKey} className="defaultText" to="/item-list">
+                {name}
               </Link>
             );
           })}
         </div>
         <div>
-          {BRAND_DATA.map((e, idx) => {
+          {BRAND_DATA.map(({ brandKey, name }) => {
             return (
-              <Link key={idx} className="defaultText" to="/item-list">
-                {e.name}
+              <Link key={brandKey} className="defaultText" to="/item-list">
+                {name}
               </Link>
             );
           })}
