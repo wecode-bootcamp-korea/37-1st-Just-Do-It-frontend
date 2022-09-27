58ac1a613b5599b1b67b948391a82e9e7bab72ab
1211lys <hpppower111@gmail.com>
2022-09-27 09:20:15 +0900

    refc 코드 수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,40 +1,40 @@
 import React, { useEffect, useState } from 'react';
 // import { MAIN_FOOTER_IMG_DATA } from './MAIN_DATA';
+import { Link } from 'react-router-dom';
+import { MAIN_CONFIG } from '../../config';
 import './Main.scss';
 
 function Main() {
   const [itemList, setItemList] = useState([]);
 
   useEffect(() => {
-    fetch('http://172.20.10.12:8000/products/?offset=0&limit=19')
+    fetch(`${MAIN_CONFIG.api}`)
       .then(response => response.json())
-      // .then(data => console.log(data))
-      .then(result => setItemList(result.list))
-      .catch(error => {
-        console.log(error);
-      });
+      .then(({ list }) => setItemList(list))
+      .catch(error => console.log(error));
   }, []);
+
   return (
     <div className="main">
-      <h1 className="videoImgTitle categoryText">
-        JUST-DO-IT 이승훈은 어떤사람일까 ?
-      </h1>
       <div className="video">
         <video muted autoPlay loop width="80%">
           <source src="/videos/nikeRun33.mp4" type="video/mp4" alt="Nike Run" />
         </video>
       </div>
       <div className="videoTextContainer">
-        <h1 className="infoHeader">이승훈의 달리기</h1>
-        <p className="videoInfo">
-          선선한 날씨를 위한 가을 컬렉션과 함께 데일리 룩을 완성해보세요.
+        <h1 className="infoHeader">JUST-DO-IT 데일리 런닝 컬렉션</h1>
+        <p className="infoMain">
+          선선한 날씨를 위한 런닝 컬렉션과 함께 데일리 룩을 완성해보세요.
         </p>
         <div className="videoButton">
-          <button className="buyButton btn">구매하기</button>
-          <button className="infoButton btn">자세히 보기</button>
+          <Link to="item-list" className="buyButton btn">
+            구매하기
+          </Link>
+          <Link to="item-list" className="infoButton btn">
+            자세히 보기
+          </Link>
         </div>
       </div>
-      <h1 className="firstImgTitle categoryText">이승훈의 신발</h1>
       <div className="firstImgContainer">
         <div className="firstImgBox" />
         <div className="secondImgBox">
@@ -43,31 +43,34 @@ function Main() {
         </div>
       </div>
       <div className="videoTextContainer">
-        <h1 className="infoHeader">이승훈의 달리기</h1>
-        <p className="videoInfo">
-          선선한 날씨를 위한 가을 컬렉션과 함께 데일리 룩을 완성해보세요.
-        </p>
+        <h1 className="infoHeader">JUST-DO-IT 팀원 사진</h1>
+        <p className="infoMain">뭐라고 쓸지 진짜 모르겠다 아.....</p>
         <div className="videoButton">
-          <button className="buyButton btn">구매하기</button>
-          <button className="infoButton btn">자세히 보기</button>
+          <Link to="item-list" className="buyButton btn">
+            구매하기
+          </Link>
+          <Link to="item-list" className="infoButton btn">
+            자세히 보기
+          </Link>
         </div>
       </div>
-      <h1 className="secondImgTitle categoryText">이승훈의 옷</h1>
       <div className="secondImgContainer">
         <div className="firstImgBox" />
         <div className="secondImgBox" />
       </div>
       <div className="videoTextContainer">
-        <h1 className="infoHeader">이승훈의 달리기</h1>
-        <p className="videoInfo">
-          선선한 날씨를 위한 가을 컬렉션과 함께 데일리 룩을 완성해보세요.
-        </p>
+        <h1 className="infoHeader">JUST-DO-IT 팀원 사진</h1>
+        <p className="infoMain">뭐라고 쓸지 진짜 모르겠다 아.....</p>
         <div className="videoButton">
-          <button className="buyButton btn">구매하기</button>
-          <button className="infoButton btn">자세히 보기</button>
+          <Link to="item-list" className="buyButton btn">
+            구매하기
+          </Link>
+          <Link to="item-list" className="infoButton btn">
+            자세히 보기
+          </Link>
         </div>
       </div>
-      <h1 className="thirdImgTitle categoryText">Featured Shoes</h1>
+      <h1 className="thirdImgTitle categoryText">추천 상품</h1>
       <div className="thirdTitle">
         {itemList.map(
           ({ id, thumbnail, productName, brandName, retailPrice }) => {
