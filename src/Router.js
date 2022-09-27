8de83a0965f922c2dd1f21b7dc1186e7ba69b101
1211lys <hpppower111@gmail.com>
2022-09-27 11:35:21 +0900

    conflict 해결

--- src/Router.js
+++ src/Router.js
@@ -9,9 +9,7 @@ function Router() {
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Main />} />
-        <Route path="/itemList" element={<ItemList />} />
       </Routes>
       <Footer />
     </BrowserRouter>
