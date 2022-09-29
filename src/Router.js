63224dc5e2dfe8f9ce7fd208cbbedd8ae0ab4d7e
0taepark <ytpark1665@gmail.com>
2022-09-29 11:20:42 +0900

    wip

--- src/Router.js
+++ src/Router.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // import Main from './pages/Main/Main';
 import ItemList from './pages/ItemsList/ItemsList';
-import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
+// import Nav from './components/Nav/Nav';
+// import Footer from './components/Footer/Footer';
 import SignUp from './pages/SignUp/SignUp.jsx';
 import WishList from './pages/wishlist/WishList';
 import ItemDetail from './pages/ItemDetail/ItemDetail';
